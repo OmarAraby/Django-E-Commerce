16c47144efa5443f4c4428d21132fe3f7ded3f27
OmarAraby <omararaby789@gmail.com>
2023-12-04 14:53:03 +0200

    Add accounnts app

--- product/views.py
+++ product/views.py
@@ -27,7 +27,7 @@ def product_detail(request,slug):
     related_products = Product.objects.filter(
         PRDCategory=product_detail.PRDCategory,
         PRDBrand=product_detail.PRDBrand
-    ).exclude(id=product_detail.id)[:4]
+    ).exclude(id=product_detail.id).order_by('-PRDCreated')[:4]
     
     
     context = {'product_detail':product_detail, 'related_products':related_products}
--- project/urls.py
+++ project/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('product.urls', namespace='product') ),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
 
 
